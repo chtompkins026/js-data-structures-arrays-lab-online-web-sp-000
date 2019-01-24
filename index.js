07a5c585cee3bca90ee98c46ca2144bf5364c8a3
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-24 19:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveLastDriver() {
   drivers.pop(); 
 }
 
-function destructivelyRemoveLastDriver() { 
-  drivers.pop(); 
+function destructivelyRemoveFirstDriver() { 
+  drivers.unshift(); 
 }
